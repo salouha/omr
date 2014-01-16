43d1f9225bd088faa26e426705fe58c833b3995d
Salah Kayali <salah.kayali@gmail.com>
2014-01-16 14:54:50 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omr::Application.config.secret_key_base = 'f2f1cd3e6847c2bf97eb1e94aa77e8b45432abc296e1909c806ee858adfe11826a2945e1a6a3dba01a8144ec402df43df93d897a5a8d3156135d60bc32ff1915'
